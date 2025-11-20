ffc06dd27707159d26c0c138e9094613608503ea
DESKTOP-NCE2VKQ\Aatir <aatir371@gmail.com>
2025-11-20 09:20:48 +0500

    sign off home

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let digit_input = [];
 let computaion_array = [];
+let computaion_string = [];
 let optr = null;
 let num1 = null;
 let num2 = null;
@@ -23,6 +24,7 @@ function calculate(number1, number2, opertor){
     switch (opertor) {
         case '%':
             // look into how this is worked
+            return (number1 % number2);
             break;
         case '/':
             return (number1 / number2);
@@ -57,7 +59,6 @@ function get_computaion_result(comp_array){
             }
             optr = comp_array[i];
             if (num2 === null){
-                // console.warn("Warning: num1 is not defined!");
                 continue;
             } else{
                 num1 = calculate(num1, num2, optr);
@@ -69,7 +70,38 @@ function get_computaion_result(comp_array){
     return calculate(num1, num2, optr);
 }
 
+function reset(){
+    digit_input = [];
+    computaion_array = [];
+    computaion_string = [];
+    optr = null;
+    // num1 = null;
+    num2 = null;
+    num1Lock = false;
+    result = null;
+}
 
+function ful_reset(){
+    reset();
+    num1 = null;
+    document.getElementById("computation").textContent = "";
+    document.getElementById("result").textContent = "0";
+
+    ////////////////
+    enableButton("equal-to-btn");
+}
+
+// Function to disable the button
+function disableButton(button_id) {
+    const button = document.getElementById(button_id);
+    button.disabled = true; // Set to true to disable
+}
+
+// Function to enable the button
+function enableButton(button_id) {
+    const button = document.getElementById(button_id);
+    button.disabled = false; // Set to false to enable
+}
 
 /////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// BUTTON RESPONSE ////////////////////////////////
@@ -78,43 +110,62 @@ function get_computaion_result(comp_array){
 
 document.querySelectorAll('.num-btn').forEach(btn => {
     btn.addEventListener('click', function () {
-        digit_input.push(this.value)
-        // console.log(digit_input)
+        digit_input.push(this.value);
+
+        // following two lines are just for display purpose
+        computaion_string.push(this.value);
+        document.getElementById("computation").textContent = computaion_string.join(" ");
+        console.log(computaion_string.join(" "));
+
+        ////////////////
+        enableButton("equal-to-btn");
     });
 });
 
 document.querySelectorAll('.optr-btn').forEach(btn => {
     btn.addEventListener('click', function () {
-        computaion_array.push(str_to_num(digit_input));
-        digit_input = []
+        if (!num1){
+            // console.log(digit_input.join(''));
+            // console.log(parseFloat(digit_input.join('')));
+            computaion_array.push(parseFloat(digit_input.join('')));
+            digit_input = []
+        }else {
+            computaion_array.push(num1);
+
+            // following two lines are just for display purpose
+            computaion_string.push(num1);
+            document.getElementById("computation").textContent = computaion_string.join(" ");
+        }
+
+        // following two lines are just for display purpose
+        computaion_string.push(this.value);
+        document.getElementById("computation").textContent = computaion_string.join(" ");
+
+        ////////////////
+        disableButton("equal-to-btn");
+        
         computaion_array.push(this.value);
+        console.log(this.value);
     });
 });
 
-document.getElementById('get-result-using-equal-to').onclick = function(){
-    computaion_array.push(str_to_num(digit_input));
+document.getElementById('equal-to-btn').onclick = function(){
+    computaion_array.push(parseFloat(digit_input.join('')));
     digit_input = []
     result = get_computaion_result(computaion_array);
     console.log("result: " + result);
     num1 = result;
+    document.getElementById("computation").textContent = result;
+    document.getElementById("result").textContent = result;
     reset();
 }
 
 document.getElementById('clear-all').onclick = function(){
-    reset();
-    num1 = null;
+    ful_reset();
 }
 
 
 
-function reset(){
-    digit_input = [];
-    computaion_array = [];
-    optr = null;
-    num1 = null;
-    num2 = null;
-    num1Lock = false;
-    result = null;
-}
 
-// controler function (js main function)
+
+// look into: controler function (js main function)
